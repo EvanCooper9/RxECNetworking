03b1ba25449235320eabeed9f88061455c87270e
EvanCooper9 <evan.cooper@rogers.com>
2021-03-19 10:50:16 -0400

    Fix RxAction

--- Sources/RxAction.swift
+++ Sources/RxAction.swift
@@ -9,8 +9,8 @@ public protocol RxRequestWillBeginAction: RxAction, RequestWillBeginAction {
     func requestWillBegin(_ request: NetworkRequest) -> Single<NetworkRequest>
 }
 
-extension RxRequestWillBeginAction {
-    public func requestWillBegin(_ request: NetworkRequest, completion: @escaping (Result<NetworkRequest, Error>) -> Void) {
+public extension RxRequestWillBeginAction {
+    func requestWillBegin(_ request: NetworkRequest, completion: @escaping (Result<NetworkRequest, Error>) -> Void) {
         requestWillBegin(request)
             .subscribe { request in
                 completion(.success(request))
@@ -27,45 +27,18 @@ public protocol RxResponseBeganAction: RxAction, ResponseBeganAction {}
 
 public protocol RxResponseCompletedAction: RxAction, ResponseCompletedAction {
     var disposeBag: DisposeBag { get }
-    func responseReceived(request: NetworkRequest, response: NetworkResponse) -> Single<NetworkResponse>
+    
+    func responseCompleted(request: NetworkRequest, response: NetworkResponse) -> Single<NetworkResponse>
 }
 
-extension RxResponseCompletedAction {
+public extension RxResponseCompletedAction {
     func responseCompleted(request: NetworkRequest, response: NetworkResponse, completion: @escaping (Result<NetworkResponse, Error>) -> Void) {
-        responseReceived(request: request, response: response)
+        responseCompleted(request: request, response: response)
             .subscribe { response in
                 completion(.success(response))
             } onFailure: { error in
                 completion(.failure(error))
             }
             .disposed(by: disposeBag)
-    }
-}
-
-struct TestRxAction {
-    let disposeBag = DisposeBag()
-}
-
-extension TestRxAction: RxRequestWillBeginAction {
-    func requestWillBegin(_ request: NetworkRequest) -> Single<NetworkRequest> {
-        .just(request)
-    }
-}
-
-extension TestRxAction: RxRequestBeganAction {
-    func requestBegan(_ request: NetworkRequest) {
-        print(#function)
-    }
-}
-
-extension TestRxAction: RxResponseBeganAction {
-    func responseBegan(request: NetworkRequest, response: NetworkResponse) {
-        print(#function)
-    }
-}
-
-extension TestRxAction: RxResponseCompletedAction {    
-    func responseReceived(request: NetworkRequest, response: NetworkResponse) -> Single<NetworkResponse> {
-        .just(response)
     }
 }
